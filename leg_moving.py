af1b9f442f3b3704bce0a2348c8e7bb449c763f6
guiljaune <guiljaune@gmail.com>
2021-03-13 18:48:41 -0500

    from pi

--- leg_moving.py
+++ leg_moving.py
@@ -81,7 +81,7 @@ while(True):
     arduino.serialSend(pulsesCommand)#send serial command to arduino
     #time.sleep(0.050)
     print(pulsesCommand)
-    
+    #test_pi
 
 
     #print(time.time() - loopTime ,T)
